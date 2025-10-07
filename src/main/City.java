19f3bdc3d3e832f888c7f041ac7f8af8af5869af
Sasha <sp.kleincharland@gmail.com>
2025-10-07 14:31:41 -0400

    implement classes Train, Segment, Connection, City, and CSVReader

--- src/main/City.java
+++ src/main/City.java
@@ -1,3 +1,13 @@
 public class City {
     
+    private String name;
+
+    public City(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
--- src/main/Segment.java
+++ src/main/Segment.java
@@ -1,3 +1,15 @@
+import java.time.Duration;
+
 public class Segment {
     
+    private Duration connectionDuration;
+
+    public Segment(Duration connectionDuration) {
+        this.connectionDuration = connectionDuration;
+    }
+
+    public Duration getConnectionDuration() {
+        return connectionDuration;
+    }
+
 }
