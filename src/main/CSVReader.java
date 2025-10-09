3600e9ea6df389e80385486dad777a328de58a0b
Priyesh Patel <priyeshpatel1216@gmail.com>
2025-10-09 14:51:40 -0400

    Minor structure change

--- src/main/CSVReader.java
+++ src/main/CSVReader.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class CSVReader {
-    
+
     private String filepath;
     private ArrayList<ArrayList<String>> data;
 
@@ -34,44 +34,57 @@ public class CSVReader {
     public void readData() {
         // Read in the contents of the CSV file into the data ArrayList
 
-        try(BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filepath), StandardCharsets.UTF_8))) {
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(new FileInputStream(filepath), StandardCharsets.UTF_8))) {
             System.out.println("Reading data");
 
-            String line;                        // One line of the CSV file, read in as a string
-            String[] splitLine;                 // The line string, split into an array
-            ArrayList<String> lineArrayList;    // The split array, converted into an ArrayList to be stored in 2D data ArrayList
-            
-            while((line = reader.readLine()) != null) {
+            String line; // One line of the CSV file, read in as a string
+            String[] splitLine; // The line string, split into an array
+            ArrayList<String> lineArrayList; // The split array, converted into an ArrayList to be stored in 2D data
+                                             // ArrayList
+
+            while ((line = reader.readLine()) != null) {
                 if (line.contains("\"")) {
-                    /* Normally we would just split one line of the CSV file on the comma characters using line.split(",");
-                     * However if the days of operation are not joined together, such as Mon-Fri, they will be listed separately in 
-                     * the format "Day1, Day2, ...". The quotation marks indicate that this should be treated as one field. 
+                    /*
+                     * Normally we would just split one line of the CSV file on the comma characters
+                     * using line.split(",");
+                     * However if the days of operation are not joined together, such as Mon-Fri,
+                     * they will be listed separately in
+                     * the format "Day1, Day2, ...". The quotation marks indicate that this should
+                     * be treated as one field.
                      * 
-                     * Just splitting the line on the comma characters will treat each of these days as a separate field,
+                     * Just splitting the line on the comma characters will treat each of these days
+                     * as a separate field,
                      * when instead we want to treat the list of days as one continuous string.
-                    */
+                     */
 
                     String[] lineSegments = line.split("\""); // Split on the quotation marks first
 
-                    // The first segment includes everything before the DaysOfOperation field, so we just split on the commas normally
+                    // The first segment includes everything before the DaysOfOperation field, so we
+                    // just split on the commas normally
                     splitLine = lineSegments[0].split(",");
                     lineArrayList = new ArrayList<String>(Arrays.asList(splitLine)); // Conver to ArrayList
 
-                    // The second segment is the DaysOfOperation field, which should be treated as one string
+                    // The second segment is the DaysOfOperation field, which should be treated as
+                    // one string
                     lineArrayList.add(lineSegments[1]);
 
-                    // The last segement is the remainder, which should be split just as the first segment
+                    // The last segement is the remainder, which should be split just as the first
+                    // segment
                     lineSegments[2] = lineSegments[2].substring(1); // Remove leading comma
                     splitLine = lineSegments[2].split(",");
-                    
-                    for (String item: splitLine) {
+
+                    for (String item : splitLine) {
                         lineArrayList.add(item);
                     }
                 } else {
-                    /* In this case, DaysOfOperation is listed in the format "BeginningDay-EndDay", or just "Daily", 
-                     * so there are no extra commas to handle. We can just split the whole line on the commas, and turn it into
+                    /*
+                     * In this case, DaysOfOperation is listed in the format "BeginningDay-EndDay",
+                     * or just "Daily",
+                     * so there are no extra commas to handle. We can just split the whole line on
+                     * the commas, and turn it into
                      * an ArrayList.
-                    */
+                     */
                     splitLine = line.split(",");
                     lineArrayList = new ArrayList<String>(Arrays.asList(splitLine));
                 }
@@ -80,10 +93,14 @@ public class CSVReader {
                 data.add(lineArrayList);
             }
 
-        } catch (FileNotFoundException e) {
+        }
+
+        catch (FileNotFoundException e) {
             System.out.println("Could not locate file.");
             System.exit(1);
-        } catch (IOException e) {
+        }
+
+        catch (IOException e) {
             System.out.println("Something went wrong.");
             System.exit(1);
         }
